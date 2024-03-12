2b9f2c4e68fcd5787770fdc72eae666a2c1b1946
Nathan Dickinson <nathandickinson04@gmail.com>
2024-03-12 12:23:24 -0400

    Changed wrong name to fix service worker not working

--- client/src-sw.js
+++ client/src-sw.js
@@ -27,7 +27,7 @@ warmStrategyCache({
 registerRoute(
   // Here we define the callback function that will filter the requests we want to cache (in this case, JS and CSS files)
   ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
-  new StaleWhileRevalidate({
+  new CacheFirst({
     // Name of the cache storage.
     cacheName: 'asset-cache',
     plugins: [
